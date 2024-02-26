2d4d339cf45096ca5e82f4da08ad5b58056940b9
jumdc <julie.mordacq@live.fr>
2024-02-26 14:41:34 +0100

    dummy updates

--- scripts/launch.sh
+++ scripts/launch.sh
@@ -1,10 +1,11 @@
 python toyxp/train-sim.py \
 ++prefix=fully-supervised \
-++self_supervised.pretrained=False \
+++self_supervised.pretrained=True \
 ++self_supervised.epochs=2 \
-++supervised.epochs=100 \
+++supervised.epochs=10 \
 ++supervised.lr=1e-4 \
-++overfit_batches=0 \
+++overfit_batches=2 \
 ++log=True \
 ++batch_size=128 \
-++num_workers=30
+++num_workers=30 \
+++log=False
